0681c9d773527645ae580b91fc8b75aa94e41cf0
Alexey Drumlevich <l-gc@yandex.ru>
2023-07-27 15:31:58 +0300

    displayDirContent

--- input-output-network/src/telran/io/InputOutputTest.java
+++ input-output-network/src/telran/io/InputOutputTest.java
@@ -2,8 +2,12 @@ package telran.io;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.IOException;
+import java.nio.file.FileVisitOption;
 import java.nio.file.Files;
+import java.nio.file.LinkOption;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -17,8 +21,12 @@ class InputOutputTest {
 	@Test
 	void pathTests() {
 		Path pathParent = Path.of("../..");
+		System.out.println(pathParent.toAbsolutePath().normalize()
+				.getName(0));
+		System.out.println(pathParent.toAbsolutePath());
 		System.out.println(pathParent.toAbsolutePath().normalize());
-				//.getName(0));
+		
+		
 	}
 	@Test
 	void displayDirContentTest() {
@@ -27,6 +35,25 @@ class InputOutputTest {
 
 	private void displayDirContent(Path dirPath, int maxDepth) {
 		// TODO Auto-generated method stub
+		
+		Path startPath = dirPath.toAbsolutePath().normalize();
+		int startPathDepth = startPath.getNameCount();
+		
+		try {
+			Files
+				.walk(startPath, maxDepth, new FileVisitOption[0])
+				.forEach(path -> System.out.printf(
+					"%s %s - %s\n",
+					"    ".repeat(path.getNameCount() - startPathDepth), 
+					path.getName(path.getNameCount() - 1), 
+					Files.isDirectory(path, new LinkOption[0]) ? "dir" : "file")
+				);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
 		//Display content directory by using method walk of the class Files
 //		simple directory name>
 //       <simple file name> - file
