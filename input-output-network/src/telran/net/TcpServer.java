815d3cd7d9b123189d6c1f337d23c8403d86f758
Alexey Drumlevich <l-gc@yandex.ru>
2023-10-12 21:08:38 +0300

    threads pool

--- input-output-network/src/telran/net/TcpServer.java
+++ input-output-network/src/telran/net/TcpServer.java
@@ -1,10 +1,18 @@
 package telran.net;
 import java.io.IOException;
 import java.net.*;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
 public class TcpServer implements Runnable {
 	private int port;
 	private ApplProtocol protocol;
 	private ServerSocket serverSocket;
+	
+	private ExecutorService executorService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+	
 	public TcpServer(int port, ApplProtocol protocol) throws IOException {
 		this.port = port;
 		this.protocol = protocol;
@@ -18,12 +26,16 @@ public class TcpServer implements Runnable {
 			while(true) {
 				Socket socket = serverSocket.accept();
 				TcpClientServer clientServer = new TcpClientServer(socket, protocol);
-				clientServer.run();
+				executorService.execute(clientServer);
 			}
 			
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
+	}
+	
+	public void shutdown() {
+		executorService.shutdown();
 	}
 
 }
